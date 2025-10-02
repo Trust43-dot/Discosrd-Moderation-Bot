26daf87600a204526f492e1dec70afeca8c2fcc9
Aditya Kr <aditya123455432@gmail.com>
2025-10-02 16:06:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Use Render’s port or fallback to 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 require('dotenv').config();
 const {
     Client,
@@ -587,3 +600,4 @@ Status: Online 🟢
 }
 );
 client.login(TOKEN);
+
